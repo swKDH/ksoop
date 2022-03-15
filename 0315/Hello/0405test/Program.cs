76dddec490bef055bd9d3aa9c88b8f6c29b349c2
swKDH <531kdh@naver.com>
2022-03-15 20:44:00 +0900

    0315

---
+++ 0315/Hello/0405test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0405test
+{
+    class A
+    {
+        public int n;
+        public A()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B : A
+    {
+        public B(int n)
+        {
+            this.n = n;
+            Console.WriteLine("B");
+        }
+        public void X()
+        {
+            Console.WriteLine("n : "+n);
+        }
+    }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            B b = new B(99);
+            b.X();
+        }
+    }
+   }
+
---
+++ 0315/Hello/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("1 3 5 7 9 ");
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i+=2)
+                Console.Write(i + " ");
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                if (i% 2 == 1)
+                    Console.Write(i + " ");
+            Console.WriteLine();
+            int j = 0;
+            while (j<10)
+            {
+                j++;
+                if (j% 2 == 0)
+                    continue;
+                Console.Write(j + " ");
+
+            }
+            
+
+
+
+
+        }
+    }
+}
