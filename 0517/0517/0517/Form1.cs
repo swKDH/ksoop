36f069dbd56ee7eaa31b940d5bf6dda00df90383
swKDH <531kdh@naver.com>
2022-05-17 23:11:00 +0900

    0517

---
+++ 0517/0517/0517/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0517
+{
+    public partial class Form1 : Form
+    {
+
+        DateTime startDateTime;
+        public DateTime GetStartDateTime()
+        {
+            return startDateTime;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = FlatStyle.Flat.ToString();
+        }
+
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to close?", "Prompt", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = FlatStyle.Popup.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = FlatStyle.Standard.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           label1.Text = FlatStyle.System.ToString();
+        }
+
+        
+    }
+}
